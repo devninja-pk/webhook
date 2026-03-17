58c207adc6f5da82232fb9466d4b95fc9b38f08d
unknown <shahzaibhamdani1214@gmail.com>
2026-03-17 15:20:29 +0500

    Upload Project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(){
+    let age=18;
+    function AgeUpdate(){
+      
+      age= 20;
+      console.log(age)
+    }
+    
+  return(
+     <>
+     <h1>My Age is {age}</h1>
+      <button onClick={AgeUpdate}>Click Me </button>
+     </>
+  )
 }
 
 export default App;
